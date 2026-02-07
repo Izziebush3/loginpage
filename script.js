4d06986fa81a63e3f204b079b52a475a6746b0e8
Izzie Bush <wisdomohia3@gmail.com>
2026-02-07 12:35:39 +0100

    Added interactivity and updated files

---
+++ script.js
@@ -0,0 +1,12 @@
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function (e) {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+});
+
+const loginBtn = document.getElementById("loginBtn");
+loginBtn.addEventListener("click", function () {
+    alert('Login successful!');
+});
